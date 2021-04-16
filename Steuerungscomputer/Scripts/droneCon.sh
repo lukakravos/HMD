f6afe48581964349f13fe35fafcb9c7ece22834f
lukakravos <45269511+lukakravos@users.noreply.github.com>
2021-04-16 02:29:59 +0200

    Delete Steuerungscomputer directory

--- Steuerungscomputer/Scripts/droneCon.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-#nohup sudo /home/pi/HMD/mavCon.sh &
-sleep 60
-#nohup sudo python3 /home/pi/HMD/getMatlabParams.py &
-sudo /home/pi/HMD/getMatlabParamsSer.py &
--- Steuerungscomputer/Scripts/mavCon.sh
+++
@@ -1,2 +0,0 @@
-mavproxy.py --out 192.168.0.157:14551 --out=udpin:192.168.0.157:14550
-#--daemon --non-interactive
